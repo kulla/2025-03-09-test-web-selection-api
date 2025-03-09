04fda46b7b6bb9c21452a13699b047181862d718
Stephan Kulla <git.mail@kulla.me>
2025-03-09 18:08:38 +0100

    feat: Add some test content

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,10 @@ import './App.css'
 const App = () => {
   return (
     <div className="content">
-      <h1>Rsbuild with React</h1>
-      <p>Start building amazing things with Rsbuild.</p>
+      <h1>Test Heading</h1>
+      <p>This is a paragraph of text to test the site.</p>
+      <p>Here is another paragraph with more information.</p>
+      <p>Feel free to add more content as needed.</p>
     </div>
   )
 }
